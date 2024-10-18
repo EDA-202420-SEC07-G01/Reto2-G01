bad396ae7ba4fb674c5e6bb2c686d7215443247e
JesusMOB <jm.otero@uniandes.edu.co>
2024-10-17 20:17:59 -0500

    correciones

--- App/view.py
+++ App/view.py
@@ -99,8 +99,7 @@ def print_req_3(control):
     """
         Función que imprime la solución del Requerimiento 3 en consola
     """
-    # TODO: Imprimir el resultado del requerimiento 3
-    pass
+    
 
 
 def print_req_4(control):
